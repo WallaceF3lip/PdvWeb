8d9ed0929e648fa05ce87e0868f48077071b8c70
F3lps <felipewallace489@gmail.com>
2024-10-01 13:50:08 -0300

    config gitignore

--- pdv.api/app/config/db.py
+++ pdv.api/app/config/db.py
@@ -1,6 +1,14 @@
+import os
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017")
+load_dotenv()  # loads the environment variables from .env
+
+MONGO_SERVE_CONNECTION_STRING = os.environ["MONGO_SERVE_CONNECTION_STRING"]
+
+# client = MongoClient("mongodb://localhost:27017")
+
+client = MongoClient(MONGO_SERVE_CONNECTION_STRING)
 
 db = client.pdv_db
 
